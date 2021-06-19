c590067912038c225fffd8bd105ae0600c8e8320
PookMook <arthur.juchereau@gmail.com>
2021-06-19 11:54:57 -0400

    chore: setup TS, eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    semi: false,
+    trailingComma: 'es5',
+    singleQuote: false,
+    printWidth: 100,
+    tabWidth: 2,
+    useTabs: false,
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import { ReactElement } from "react"
+
+export default function IndexPage(): ReactElement {
+  return <h1>Hello World</h1>
+}
