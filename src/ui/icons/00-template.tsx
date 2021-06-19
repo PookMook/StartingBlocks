109c3178b91fa22f29ae58a0ac8d5d226b949630
PookMook <arthur.juchereau@gmail.com>
2021-06-19 17:01:27 -0400

    chore: variable stroke color

--- src/ui/icons/00-template.tsx
+++ src/ui/icons/00-template.tsx
@@ -6,8 +6,8 @@ export function TemplateIcon(props: props): ReactElement {
   return (
     <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" {...props}>
       <g>
-        <line y2="512" x2="512" y1="0" x1="0" stroke="#000" />
-        <line y2="512" x2="0" y1="0" x1="512" stroke="#000" />
+        <line y2="512" x2="512" y1="0" x1="0" />
+        <line y2="512" x2="0" y1="0" x1="512" />
       </g>
     </SVG>
   )
