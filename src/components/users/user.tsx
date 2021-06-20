c0811720c8c5ff56b9e7f51055cf1dc0950c0361
PookMook <arthur.juchereau@gmail.com>
2021-06-20 11:32:32 -0400

    chore: is this combo of version working?

--- src/components/users/user.tsx
+++ src/components/users/user.tsx
@@ -1,8 +1,12 @@
-//import { useLoadAllUsersQuery } from "generated/graphql"
+import { useLoadAllUsersQuery } from "generated/graphql"
 import { ReactElement } from "react"
 import { Pre } from "ui/pre"
 
 export function Users(): ReactElement {
-  // const { data, status } = useLoadAllUsersQuery()
-  return <Pre Type="primary">Please prisma, build your client</Pre>
+  const { data, status } = useLoadAllUsersQuery()
+  return (
+    <Pre Type="primary">
+      {status}: {JSON.stringify(data, null, 1)}
+    </Pre>
+  )
 }
