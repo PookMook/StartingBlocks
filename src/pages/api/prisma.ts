7266d8984dbb7bffc696a7537b5c8d1c5202ae9a
PookMook <arthur.juchereau@gmail.com>
2021-06-20 22:26:28 -0400

    add route not gql

---
+++ src/pages/api/prisma.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { PrismaClient } from "@prisma/client"
+
+let prisma: PrismaClient
+// add prisma to the NodeJS global type
+
+interface CustomNodeJsGlobal extends NodeJS.Global {
+  prisma: PrismaClient
+}
+
+declare const global: CustomNodeJsGlobal
+
+// Prevent multiple instances of Prisma Client in development
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  prisma = global.prisma
+}
+
+
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const users = await prisma.user.findMany()
+  res.status(200).json(users)
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,22 @@ import { ReactElement, useEffect, useState } from "react"
 
 export default function IndexPage(): ReactElement {
 
-  const [response, setResponse] = useState<string|null>(null)
+  const [response, setResponse] = useState<string|null>("API waiting")
+  const [responseGQL, setResponseGQL] = useState<string|null>("GQL waiting")
+
 
   useEffect(()=>{
     fetch('/api/graphql',{
       method: 'POST',
       body: JSON.stringify({query:`query{users{id name}}`})
     })
+    .then(res => res.text())
+    .then(text => setResponseGQL(text))
+    .catch((e)=> setResponseGQL('error'+e))
+  },[])
+
+  useEffect(()=>{
+    fetch('/api/prisma')
     .then(res => res.text())
     .then(text => setResponse(text))
     .catch((e)=> setResponse('error'+e))
