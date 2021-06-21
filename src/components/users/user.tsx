af503ee6789dcdcbe6272afc1dfa43d4f7b1386d
PookMook <arthur.juchereau@gmail.com>
2021-06-21 16:07:54 -0400

    chore: yes you can lazy load

--- src/components/users/user.tsx
+++ src/components/users/user.tsx
@@ -1,9 +1,18 @@
 import { useLoadAllUsersQuery } from "generated/graphql"
-import { ReactElement } from "react"
+import { ReactElement, useEffect } from "react"
 import { Pre } from "ui/pre"
 
 export function Users(): ReactElement {
-  const { data, status } = useLoadAllUsersQuery()
+  const { data, status, refetch } = useLoadAllUsersQuery(undefined, { enabled: false })
+
+  useEffect(() => {
+    setTimeout(() => refetch(), 3000)
+  }, [])
+
+  if (status === "idle") {
+    return <p>Waiting 3 seconds before fetching data</p>
+  }
+
   return (
     <Pre Type="primary">
       {status}: {JSON.stringify(data, null, 1)}
