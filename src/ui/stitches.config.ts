01caf2e2311d089a60ec51e93c8e70aa84c23023
PookMook <arthur.juchereau@gmail.com>
2021-08-13 09:48:46 -0400

    chore: prepare token for UI

--- src/ui/stitches.config.ts
+++ src/ui/stitches.config.ts
@@ -1,12 +1,77 @@
 import { createStitches } from "@stitches/react"
-import { violet, violetDark, slate, slateDark } from "@radix-ui/colors"
+import {
+  violet,
+  violetDark,
+  mauve,
+  mauveDark,
+  red,
+  redDark,
+  tomato,
+  tomatoDark,
+  purple,
+  purpleDark,
+} from "@radix-ui/colors"
+
+/*
+  scale0: excagerated background
+  scale1: app background
+  scale2: subtle background
+  scale3: UI element background
+  scale4: Hovered UI element background
+  scale5: Active/selected UI element background
+  scale6: Subtle borders and separators
+  scale7: UI element border + focus ring
+  scale8: Hovered UI element border
+  scale9: Solid background / highest chroma
+  scale10: Hovered Solid background / lowest contrast
+  scale11: Low Contrast
+  scale12: High Contrast
+  scale13: blackest Contrast
+*/
 
 export const { styled, getCssText, keyframes, createTheme } = createStitches({
   theme: {
     colors: {
       ...violet,
-      ...slate,
-      primary: "blue",
+      ...mauve,
+      ...tomato,
+      ...red,
+      ...purple,
+
+      successSolidBackground: "$violet9",
+      infoSolidBackground: "$purple9",
+      warningSolidBackground: "$tomato9",
+
+      errorSolidBackground: "$red9",
+      errorText: "$red9",
+      errorBorder: "$red9",
+
+      appBackground: "$mauve2",
+      subtleBackground: "$mauve1",
+      subtleBackgroundColour: "$violet1",
+
+      linkText: "$violet11",
+      lowContrast: "$mauve11",
+      lowestContrast: "$mauve10",
+      placeholderText: "$mauve7",
+
+      border: "$mauve7",
+      borderFocus: "$violet7",
+
+      borderDisabled: "$mauve6",
+      disabledBackground: "$mauve2",
+      disabledText: "$mauve10",
+
+      primarySolidBackground: "$violet9",
+      primaryFaded: "$violet8",
+      primaryIcon: "$violet9",
+
+      constructiveSolidBackground: "$violet9",
+      constructiveFaded: "$violet8",
+
+      tableHeaderBackground: "$violet5",
+      tableRowBackground: "$slate3",
+      tableRowAlternateBackground: "$violet3",
     },
     fontSizes: {
       smallExtra: "0.618rem",
@@ -30,6 +95,9 @@ export const { styled, getCssText, keyframes, createTheme } = createStitches({
 export const darkTheme = createTheme({
   colors: {
     ...violetDark,
-    ...slateDark,
+    ...mauveDark,
+    ...tomatoDark,
+    ...redDark,
+    ...purpleDark,
   },
 })
